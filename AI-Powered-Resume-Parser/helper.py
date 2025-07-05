6bbba42f6d6981430c07c74105440ac54db122af
Marcos Nunes <marcosassisnunes10@gmail.com>
2025-07-05 18:27:36 +0100

    feat: add helper functions for PDF processing and data extraction
    This commit introduces a new "helper.py" module containing utility functions to support the CV analysis workflow.
    
    The key functionalities include:
    
    - PDF Text Extraction: The "read_pdf" function reads a PDF file and extracts its text content, making it available for processing.
    
    - PDF Path Retrieval: The "get_pdf_paths" function scans a directory and returns a list of all PDF file paths, enabling batch processing.
    
    - Data Extraction and Validation: The "extract_data_analysis" function parses a summarized CV, extracts key information (name, skills, education, languages, etc) using regular expressions, and validates that essential fields are not empty before creating an "Analysis" object.

---
+++ AI-Powered-Resume-Parser/helper.py
@@ -0,0 +1,70 @@
+import re, uuid, os
+import fitz
+from models.analysis import Analysis
+
+def read_pdf(file_path):
+    """
+    Extracts text content from a PDF file.
+    """
+    text = ""
+    with fitz.open(file_path) as pdf:
+        for page in pdf:
+            text += page.get_text()
+    
+    return text
+
+def get_pdf_paths(dir):
+    """
+    Retrieves the full paths of all PDF files within a specified directory.
+    """
+    pdf_paths = []
+    
+    for filename in os.listdir(dir):
+        if filename.endswith('.pdf'):
+            file_path = os.path.join(dir, filename)
+            pdf_paths.append(file_path)
+            
+    return pdf_paths
+    
+def extract_data_analysis(resum_cv, job_id, resum_id, score):
+    """
+    Parsis a summarized CV, extracts key sections using regular expressions,
+    and validates the data before creating an Analysis object.
+    """
+    sections_dict = {
+        "id": str(uuid.uuid4()),
+        "job_id": job_id,
+        "resum_id": resum_id,
+        "name": "",
+        "skills": [],
+        "education": [],
+        "languages": [],
+        "score": score
+    }
+
+    patterns = {
+        "name": r"(?:## Full Name\s*|Full Name\s*|\s*Valor\s*|\s*\S*\s*|\s*)(.*)",
+        "skills": r"## Skills\s*([\s\S]*?)(?=##|$)",
+        "education": r"## Education\s*([\s\S]*?)(?=##|$)",
+        "languages": r"## Language\s*([\s\S]*?)(?=##|$)",
+    }
+    
+    def clean_string(string: str) -> str:
+        """Removes markdown characters and extra whitespace from a string."""
+        return re.sub(r"[\*\-]+", "", string).strip()
+
+    for section, pattern in patterns.items():
+        match = re.search(pattern, resum_cv)
+        if match:
+            if section == "name":
+                sections_dict[section] = clean_string(match.group(1))
+            else:
+                # Extracts and cleans list items from a matched section
+                sections_dict[section] = [clean_string(item) for item in match.group(1).split('\n') if item.strip()]
+                    
+    # Validation to ensure that no required sections are empty
+    for key in ["name", "education", 'skills']:
+        if not sections_dict[key] or (isinstance(sections_dict[key], list) and not any(sections_dict[key])):
+            raise ValueError(f"The section '{key}' cannot be empty or contain only empty strings.")
+
+    return Analysis(**sections_dict)
